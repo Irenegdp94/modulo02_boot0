54bf94db3e0cd034f77f644284342c3649d54520
Irenegdp94 <77295901+Irenegdp94@users.noreply.github.com>
2021-01-13 18:32:23 +0100

    funciones de primer nivel

---
+++ funcionprimernivel.py
@@ -0,0 +1,14 @@
+def normal(x):
+    return x
+def cuadrado(x):
+    return x * x
+
+def sumatodos(limitTo, f):
+    resultado = 0
+    for i in range(limitTo+1):
+        resultado += f(i)
+        
+    return resultado
+
+print(sumatodos(100, normal))
+print(sumatodos(3,cuadrado))
